86cd5816d1759cfb7b1cd9f61814556808c438ed
Nathaniel Giron <nathaniel.giron@gmail.com>
2016-03-29 17:13:13 -0700

    finished activity

--- js/script.js
+++ js/script.js
@@ -1,3 +1,21 @@
 $(document).ready(function() {
   //code here
+
+  var quotes = [
+  	"I have a very bad feeling about this.",
+  	"Do. Or do not. There is no try.",
+  	"Don't get technical with me.",
+  	"Somebody has to save our skins.",
+  	"We're doomed.",
+  	"It's a trap!",
+  	"No. I am your Father.",
+  	"Aren't you a little short for a stormtrooper?",
+  	"Let the Wookiee win.",
+  	"Remember... the Force will be with you, always.",
+  ];
+
+  $('button').click(function(){
+  	var x = Math.floor((Math.random() * 9));
+    $('#quote-box').html(quotes[x]);
+  });
 });
